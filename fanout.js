1ccf1a24baf2d5673b1b7bda988cb8d2116c9a8c
Hylke Bons <hylkebons@gmail.com>
2011-07-01 23:38:29 +0100

    clean up coding style some more

--- fanout.js
+++ fanout.js
@@ -8,127 +8,131 @@
 //
 //   MIT Licensed
 
+// Usage: subscribe <channel>
+//        unsubscribe <channel>
+//        announce <channel> <message>
+//        ping
+
 
 var tcp = require("net"),
     sys = require("sys");
 
 // The .bind method from Prototype.js 
 Function.prototype.bind = function(){ 
-  var fn = this, args = Array.prototype.slice.call(arguments), object = args.shift(); 
-  return function(){ 
-    return fn.apply(object, 
-                    args.concat(Array.prototype.slice.call(arguments))); 
-  }; 
+    var fn = this, args = Array.prototype.slice.call(arguments), object = args.shift(); 
+    return function() { 
+        return fn.apply(object, args.concat(Array.prototype.slice.call(arguments))); 
+    };
 };
 
 Array.prototype.remove = function(e) {
-  for(var i = 0; i < this.length; i++)
-    if(e == this[i]) this.splice(i, 1);
+    for(var i = 0; i < this.length; i++)
+        if(e == this[i]) this.splice(i, 1);
 };
 
 // Array Remove - By John Resig (MIT Licensed)
 Array.remove = function(array, from, to) {
-  var rest = array.slice((to || from) + 1 || array.length);
-  array.length = from < 0 ? array.length + from : from;
-  return array.push.apply(array, rest);
+    var rest = array.slice((to || from) + 1 || array.length);
+    array.length = from < 0 ? array.length + from : from;
+    return array.push.apply(array, rest);
 };
 
 var msgEmitter = new process.EventEmitter();
 
 var handleMessage = function handleMessage(conn, socket, data) {
-  sys.puts('[' + conn.name + ']' + ' data: ' + data);
-  if (data == "time") {
-    socket.write(Date.now() + "\n");
-  }
+    sys.puts('[' + conn.name + ']' + ' data: ' + data);
 
-  if (data.indexOf("subscribe ") == 0) {
-    conn.addchannel(data.split(' ')[1]);
-    conn.subscribe();
-  } else if (data.indexOf("unsubscribe ") == 0) {
-    conn.removechannel(data.split(' ')[1]);
-    // update subscriptions by calling subscribe
-    conn.subscribe();
-  } else if (data.indexOf("announce ") == 0) {
-    data = data.substring(9);
-    var pos = data.indexOf(' ');
-    var channel = data.slice(0, pos);
-    var msg = data.slice(pos + 1);
-    handleControllerMessage(socket, channel, msg); 
-  }
+    if (data == "ping") {
+       socket.write(Date.now() + "\n");
+
+    } else if (data.indexOf("subscribe ") == 0) {
+       conn.addchannel(data.split(' ')[1]);
+       conn.subscribe();
+
+    } else if (data.indexOf("unsubscribe ") == 0) {
+       conn.removechannel(data.split(' ')[1]);
+
+       // update subscriptions by calling subscribe
+       conn.subscribe();
+
+    } else if (data.indexOf("announce ") == 0) {
+        data = data.substring(9);
+        var pos = data.indexOf(' ');
+        var channel = data.slice(0, pos);
+        var msg = data.slice(pos + 1);
+        handleControllerMessage(socket, channel, msg); 
+    }
 };
 
 var handleControllerMessage = function handleControllerMessage(socket, channel, data) {
-  msgEmitter.emit(channel, channel, data);
+    msgEmitter.emit(channel, channel, data);
 };
 
 function Client(connection) {
-  this.socket = connection;
-  this.name = null;
-  this.timer = null;
-  this.channels = [];
-  this.listeners = [];
-
+    this.socket    = connection;
+    this.name      = null;
+    this.timer     = null;
+    this.channels  = [];
+    this.listeners = [];
 }
 
-/* adds channel. must use "subscribe" to take effect */
+// adds channel. must use "subscribe" to take effect
 Client.prototype.addchannel = function(channel) {
-  sys.puts('adding sub: ' + channel);
+    sys.puts('adding sub: ' + channel);
 
-  this.removechannel(channel);
-  this.channels.push(channel);
+    this.removechannel(channel);
+    this.channels.push(channel);
 };
 
-/* removes channel. also removes associated listener immediately */
+// removes channel. also removes associated listener immediately
 Client.prototype.removechannel = function(channel) {
-  //sys.puts('removing sub');
-  
-  /* remove channel if it exists */
-  this.channels.remove(channel);
-  
-  /* remove listener */
-  var listener = this.listeners[channel];
-  
-  if (listener) {
-    msgEmitter.removeListener(channel, listener);
-  }
-  
+    sys.puts('removing sub');
+    
+    // remove channel if it exists
+    this.channels.remove(channel);
+    
+    // remove listener
+    var listener = this.listeners[channel];
+    
+    if (listener)
+        msgEmitter.removeListener(channel, listener);
 };
 
 Client.prototype.subscribe = function() {
-  sys.puts('subs:' + JSON.stringify(this.channels));
-  this.channels.forEach(function(channel) {
-      var listener = this.listeners[channel];
-        
-      if (listener) {
-        msgEmitter.removeListener(channel, listener);
-      }
-    }.bind(this));
-  this.listeners = [];
-  this.channels.forEach(function(channel) {
-      var listener = function(c, msg) {
-        this.socket.write(c + "!" + msg + "\n");
-      }.bind(this);
-      this.listeners[channel] = listener;
-      msgEmitter.addListener(channel, listener);
+    sys.puts('subs:' + JSON.stringify(this.channels));
+
+    this.channels.forEach(function(channel) {
+        var listener = this.listeners[channel];
+          
+        if (listener)
+          msgEmitter.removeListener(channel, listener);
+      }.bind(this));
+
+    this.listeners = [];
+    this.channels.forEach(function(channel) {
+        var listener = function(c, msg) {
+          this.socket.write(c + "!" + msg + "\n");
+        }.bind(this);
+        this.listeners[channel] = listener;
+        msgEmitter.addListener(channel, listener);
     }.bind(this));
 };
 
 Client.prototype.deconstruct = function() {
-  this.channels.forEach(function(channel) {
-      var listener = this.listeners[channel];
-      if (listener) {
-        msgEmitter.removeListener(channel, listener);
-      }
+    this.channels.forEach(function(channel) {
+        var listener = this.listeners[channel];
+
+        if (listener)
+            msgEmitter.removeListener(channel, listener);
     }.bind(this));
 };
 
 var connections = [];
-var cnt = 0;
 
 var server = tcp.createServer(function(socket) {
     var conn = new Client(socket);
     connections.push(conn);
-    conn.name = ++cnt;
+    conn.name = connections.length;
     socket.setTimeout(0);
     socket.setNoDelay();
     socket.setEncoding("utf8");
@@ -142,9 +146,9 @@ var server = tcp.createServer(function(socket) {
     });
 
     socket.addListener("data", function(data) {
-        //sys.puts('raw data ' + data);
         var dataarr = data.split("\n");
         var l = dataarr.length;
+
         for (var jj = 0; jj < dataarr.length - 1; jj++) {
           var dataline = dataarr[jj];
           handleMessage(conn, socket, dataline);
@@ -189,8 +193,8 @@ var controller = tcp.createServer(function(socket) {
           var msg = dataline.slice(pos + 1);
           handleControllerMessage(socket, channel, msg);
         }
-      });
-  });
+    });
+});
 
 var client_port = 8880;
 var controller_port = 8890;
